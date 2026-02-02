ad2517b399053c995c409b96c67a97c98a505dfd
iqqdq <iqqddq@gmail.com>
2026-02-02 21:46:05 +0300

    update app_constants.dart

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,15 +1,15 @@
 abstract class AppConstants {
-  static const appID = "6746335079";
-  static const apphudId = "app_3gg7S2FEFXP94j6pWowgKTbqJzd9B8";
+  static const appID = "";
+  static const apphudId = "";
 
-  static const weekSubID = "driver_test_week";
-  static const weekTrialSubID = "driver_test_trial_week";
-  static const lifeTimeSubID = "one_time_purchase";
+  static const weekSubID = "";
+  static const weekTrialSubID = "";
+  static const lifeTimeSubID = "";
 
   static const termsOfUse =
-      "https://docs.google.com/document/d/1J7hgske8T9km3vKS1fMTsPXUaNaW-Zc1A1-PCTNA6G8/edit?usp=sharing";
+      "";
   static const privacyUrl =
-      "https://docs.google.com/document/d/1dIIsC-PdUAvE3uo1sy8-JRmFmfGbGMp1sHu2u0Ly4D8/edit?usp=sharing";
-  static const supportUrl = "https://forms.gle/7QN32mss3uMEecJRA";
-  static const shareUrl = 'https://apps.apple.com/app/id$appID';
+      "";
+  static const supportUrl = "";
+  static const shareUrl = '';
 }
